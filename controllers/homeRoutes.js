7fa643810d6f88978926e673c50b539931e14786
lilys0 <lsouryaphanh@gmail.com>
2021-11-30 21:14:52 +1100

    logout route fix

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,29 @@ const router = require('express').Router();
 const { Review, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+  try {
+    // Get all articles and JOIN with user data
+    const reviewData = await Review.findAll({
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+
+    const reviews = reviewData.map((review) => review.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', {
+      reviews,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/review', async (req, res) => {
   try {
     // Get all reviews and JOIN with user data
