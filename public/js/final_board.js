ed6422ab72d566533abf08b64986939f26246d87
lilyso <86135192+lilyso@users.noreply.github.com>
2021-12-02 15:06:00 +1100

    Update final_board.js

--- public/js/final_board.js
+++ public/js/final_board.js
@@ -1,108 +1,110 @@
+let newReview_data = {};
 const add_pin_modal = document.querySelector('.add_pin_modal');
 
 document.querySelector('.add_pin').addEventListener('click', () => {
-    add_pin_modal.style.opacity = 1;
-    add_pin_modal.style.pointerEvents = 'all';
+  add_pin_modal.style.opacity = 1;
+  add_pin_modal.style.pointerEvents = 'all';
 });
 
-document.querySelector('.add_pin_modal').addEventListener('click', event => {
-    if (event.target === add_pin_modal) {
-        reset_modal();
-    }
+document.querySelector('.add_pin_modal').addEventListener('click', (event) => {
+  if (event.target === add_pin_modal) {
+    reset_modal();
+  }
 });
 
 let pinImageBlob = null;
 
-document.querySelector('#upload_img').addEventListener('change', event => {
-    if (event.target.files && event.target.files[0]) {
-        if (/image\/*/.test(event.target.files[0].type)) {
-            const reader = new FileReader();
+document.querySelector('#upload_img').addEventListener('change', (event) => {
+  if (event.target.files && event.target.files[0]) {
+    if (/image\/*/.test(event.target.files[0].type)) {
+      const reader = new FileReader();
 
-            reader.onload = function () {
-                const new_image = new Image();
+      reader.onload = function () {
+        const new_image = new Image();
 
-                new_image.src = reader.result;
-                pinImageBlob = reader.result;
+        new_image.src = reader.result;
+        pinImageBlob = reader.result;
 
-                new_image.onload = function () {
-                    const modals_pin = document.querySelector('.add_pin_modal .modals_pin');
+        new_image.onload = function () {
+          const modals_pin = document.querySelector(
+            '.add_pin_modal .modals_pin'
+          );
 
-                    new_image.classList.add('pin_max_width');
+          new_image.classList.add('pin_max_width');
 
-                    document.querySelector('.add_pin_modal .pin_image').appendChild(new_image);
-                    document.querySelector('#upload_img_label').style.display = 'none';
+          document
+            .querySelector('.add_pin_modal .pin_image')
+            .appendChild(new_image);
+          document.querySelector('#upload_img_label').style.display = 'none';
 
-                    modals_pin.style.display = 'block';
+          modals_pin.style.display = 'block';
 
-                    if (
-                        new_image.getBoundingClientRect().width < new_image.parentElement.getBoundingClientRect().width ||
-                        new_image.getBoundingClientRect().height < new_image.parentElement.getBoundingClientRect().height
-                    ) {
-                        new_image.classList.remove('pin_max_width');
-                        new_image.classList.add('pin_max_height');
-                    }
+          if (
+            new_image.getBoundingClientRect().width <
+              new_image.parentElement.getBoundingClientRect().width ||
+            new_image.getBoundingClientRect().height <
+              new_image.parentElement.getBoundingClientRect().height
+          ) {
+            new_image.classList.remove('pin_max_width');
+            new_image.classList.add('pin_max_height');
+          }
 
-                    modals_pin.style.opacity = 1;
-                }
-            }
+          modals_pin.style.opacity = 1;
+        };
+      };
 
-            reader.readAsDataURL(event.target.files[0]);
-        }
+      reader.readAsDataURL(event.target.files[0]);
     }
+  }
 
-    document.querySelector('#upload_img').value = '';
+  document.querySelector('#upload_img').value = '';
 });
 
 document.querySelector('.save_pin').addEventListener('click', () => {
-    const users_data = {
-        author: 'Alexis',
-        board: 'default',
-        brand: document.querySelector('#pin_title').value,
-        productType: document.querySelector('#pin_product_type').value,
-        description: document.querySelector('#pin_description').value,
-        price: document.querySelector('#pin_description').value,
-        destination: document.querySelector('#pin_product_price').value,
-        imgBlob: pinImageBlob,
-        pinSize: document.querySelector('#pin_size').value
-    }
-
-    create_pin(users_data);
-    reset_modal();
+  newReview_data = {
+    author: 'Alexis',
+    board: 'default',
+    brand: document.querySelector('#pin_title').value,
+    productType: document.querySelector('#pin_product_type').value,
+    description: document.querySelector('#pin_description').value,
+    price: document.querySelector('#pin_product_price').value,
+    destination: document.querySelector('#pin_destination').value,
+    imgBlob: pinImageBlob,
+    pinSize: document.querySelector('#pin_size').value,
+  };
+  console.log(newReview_data);
+  saveReview(newReview_data);
+  create_pin(newReview_data);
+  reset_modal();
 });
 
-
 function create_pin(pin_details) {
-    const new_pin = document.createElement('div');
-    const new_image = new Image();
+  const new_pin = document.createElement('div');
+  const new_image = new Image();
 
-    new_image.src = pin_details.imgBlob;
-    new_pin.style.opacity = 1;
+  new_image.src = pin_details.imgBlob;
+  new_pin.style.opacity = 1;
 
-    new_image.onload = function () {
-        new_pin.classList.add('card');
-        new_pin.classList.add(`card_${pin_details.pinSize}`);
-        new_image.classList.add('pin_max_width');
+  new_image.onload = function () {
+    new_pin.classList.add('card');
+    new_pin.classList.add(`card_${pin_details.pinSize}`);
+    new_image.classList.add('pin_max_width');
 
-        new_pin.innerHTML = `<div class="pin_title">${pin_details.brand}</div>
+    new_pin.innerHTML = `<div id="pin_title" data-brand='${newReview_data.brand}'></div>
+    <div id="pin_product_type" data-type='${newReview_data.productType}'></div>
+    <div id="pin_product_price" data-price='${newReview_data.price}'></div>
+    <div id="pin_description" data-descr='${newReview_data.description}'></div>
 <div class="pin_modal">
-    <div class="modal_head">
-        <div class="save_card">Save</div>
-    </div>
 
     <div class="modal_foot">
-        <div class="destination">
-            <div class="pint_mock_icon_container">
-                <img src="../assets/upper-right-arrow.png" alt="destination" class="pint_mock_icon">
+      <div class="pint_mock_icon_container">
+        <div id="pin_destination" class="destination">
+          <a href='${newReview_data.destination}' target='_blank'><img
+                    src='../assets/upper-right-arrow.png'
+                    alt='destination'
+                    class='pint_mock_icon'
+                  /></a>
             </div>
-            <span>${pin_details.destination}</span>
-        </div>
-
-        <div class="pint_mock_icon_container">
-            <img src="../assets/send.png" alt="send" class="pint_mock_icon">
-        </div>
-
-        <div class="pint_mock_icon_container">
-            <img src="../assets/ellipse.png" alt="edit" class="pint_mock_icon">
         </div>
     </div>
 </div>
@@ -110,67 +112,63 @@ function create_pin(pin_details) {
 <div class="pin_image">
 </div>`;
 
-        document.querySelector('.pin_container').appendChild(new_pin);
-        new_pin.children[2].appendChild(new_image);
+    document.querySelector('.pin_container').appendChild(new_pin);
+    new_pin.children[2].appendChild(new_image);
 
-        if (
-            new_image.getBoundingClientRect().width < new_image.parentElement.getBoundingClientRect().width ||
-            new_image.getBoundingClientRect().height < new_image.parentElement.getBoundingClientRect().height
-        ) {
-            new_image.classList.remove('pin_max_width');
-            new_image.classList.add('pin_max_height');
-        }
-
-        new_pin.style.opacity = 1;
+    if (
+      new_image.getBoundingClientRect().width <
+        new_image.parentElement.getBoundingClientRect().width ||
+      new_image.getBoundingClientRect().height <
+        new_image.parentElement.getBoundingClientRect().height
+    ) {
+      new_image.classList.remove('pin_max_width');
+      new_image.classList.add('pin_max_height');
     }
+
+    new_pin.style.opacity = 1;
+  };
 }
 
-const saveReview = async (event) => {
+const saveReview = async (newReview_data) => {
+  const brand = newReview_data.brand;
+  const type = newReview_data.productType;
+  const price = newReview_data.price;
+  const description = newReview_data.description;
+  const link = newReview_data.destination;
+  if (brand && type && price && description && link) {
+    const response = await fetch('/api/reviews', {
+      method: 'POST',
+      body: JSON.stringify({ brand, type, price, description, link }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 
-    const brand = document.querySelector('#pin_title').value.trim();
-    const type = document.querySelector('#pin_product_type').value.trim();
-    const price = document.querySelector('#pin_description').value.trim();
-    const description = document.querySelector('#pin_description').value.trim();
-    const destination = document.querySelector('#pin_product_price').value.trim();
-    if (brand && type && price && description && destination) {
-        const response = await fetch(`/api/profile`, {
-            method: 'POST',
-            body: JSON.stringify({ brand, type, price, description, destination }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        if (response.ok) {
-            document.location.replace('/profile');
-        } else {
-            alert('Failed to create product');
-        }
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create review');
     }
+  }
 };
 
-
 function reset_modal() {
-    const modals_pin = document.querySelector('.add_pin_modal .modals_pin');
+  const modals_pin = document.querySelector('.add_pin_modal .modals_pin');
 
-    add_pin_modal.style.opacity = 0;
-    add_pin_modal.style.pointerEvents = 'none';
-    document.querySelector('#upload_img_label').style.display = 'block';
-    modals_pin.style.display = 'none';
-    modals_pin.style.opacity = 0;
+  add_pin_modal.style.opacity = 0;
+  add_pin_modal.style.pointerEvents = 'none';
+  document.querySelector('#upload_img_label').style.display = 'block';
+  modals_pin.style.display = 'none';
+  modals_pin.style.opacity = 0;
 
-    if (modals_pin.children[0].children[0]) {
-        modals_pin.children[0].removeChild(modals_pin.children[0].children[0]);
-    }
-    document.querySelector('#pin_title').value = '';
-    document.querySelector('#pin_product_type').value = '';
-    document.querySelector('#pin_description').value = '';
-    document.querySelector('#pin_product_price').value = '';
-    document.querySelector('#pin_destination').value = '';
-    document.querySelector('#pin_size').value = '';
-    pinImageBlog = null;
+  if (modals_pin.children[0].children[0]) {
+    modals_pin.children[0].removeChild(modals_pin.children[0].children[0]);
+  }
+  document.querySelector('#pin_title').value = '';
+  document.querySelector('#pin_product_type').value = '';
+  document.querySelector('#pin_description').value = '';
+  document.querySelector('#pin_product_price').value = '';
+  document.querySelector('#pin_destination').value = '';
+  document.querySelector('#pin_size').value = '';
+  pinImageBlog = null;
 }
-
-document
-    .querySelector('.save_pin')
-    .addEventListener('submit', saveReview);
